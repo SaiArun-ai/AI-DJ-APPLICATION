f60e3523ee96624fb03da800519e45c2b7ae56f3
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-09-01 20:49:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 var song = "";
+var leftX;
+var leftY;
+var rightX;
+var rightY;
 function preset(){
     song = loadSound("music.mp3");
 }
 function setup(){
     canvas = createCanvas(500,450);
     canvas.position(525,150);
-    song.play();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video,pose);
+    poseNet.on("pose",gotPose);
+}
+function pose(){
+    console.log("I Work");
 }
 function draw(){
     image(video,0,0,0,0);
 }
 function Play(){
-
+    console.log("Works");
+    song.play();
+    song.rate(1);
+    song.setVolume(1);
+}
+function gotPose(error,result){
+    if(error){
+        //console.error(error);
+    }else{
+        //console.log(result);
+        leftX = result[0].pose.leftWrist.x;
+        leftY = result[0].pose.leftWrist.y;
+        rightX = result[0].pose.rightWrist.x;
+        rightY = result[0].pose.rightWrist.y;
+    }
 }
